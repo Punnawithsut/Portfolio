a13e75eec5fdbb61c8f8912396f79ec9a46b47bb
PunnawithSut <punnawith05@gmail.com>
2026-05-25 21:28:18 +0700

    fix: add correct style for radix ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ function ListItem({
       <NavigationMenuLink asChild>
         <Link 
             href={href}
-            className="block select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-secondary hover:text-secondary-foreground focus:bg-secondary focus:text-secondary-foreground"
+            className="block select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-secondary hover:text-secondary-foreground focus:bg-secondary focus:text-secondary-foreground cursor-pointer"
         >
           <div className="flex flex-col gap-1 text-sm transition hover:bg-grey">
             <div className="leading-none font-extrabold mb-1">{title}</div>
@@ -36,40 +36,34 @@ function ListItem({
 export function NavigationBar() {
     return (
         <header className="fixed top-0 left-0 z-50 w-full mt-4 px-6">
-            <div className="flex items-center justify-between mx-auto max-w-5xl">
-                <div className="text-xl font-bold">
+            <div className="flex w-full items-center justify-between">  
+                <div className="text-2xl font-bold ml-10">
                     Pun.dev
                 </div>
-                <NavigationMenu delayDuration={0}>
-                    <NavigationMenuList className="gap-10">
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="w-66">
-                                    <ListItem href="/" title="Introduction">
-                                        placeholder
-                                    </ListItem>
-                                    <ListItem href="/skills" title="My Skills">
-                                        placeholder
-                                    </ListItem>
-                                    <ListItem href="/contribution" title="My Contribution">
-                                        placeholder
-                                    </ListItem>
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild>
-                                <Link href="my-projects">Projects</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild>
-                                <Link href="my-projects">Contacts</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
+
+                <div className="flex items-center gap-4 mr-10">
+                    <NavigationMenu delayDuration={0}>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger className="text-0.75xl">Home</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <ul className="w-66 p-1">
+                                        <ListItem href="/" title="Introduction">placeholder</ListItem>
+                                        <ListItem href="/skills" title="My Skills">placeholder</ListItem>
+                                        <ListItem href="/contribution" title="My Contribution">placeholder</ListItem>
+                                    </ul>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                    
+                    <Link href="/my-projects" className="text-0.75xl font-medium transition-colors hover:bg-secondary px-4 py-2 rounded-lg">
+                        Projects & Experiences
+                    </Link>
+                    <Link href="/contacts" className="text-0.75xl font-medium transition-colors hover:bg-secondary px-4 py-2 rounded-lg">
+                        Contacts
+                    </Link>
+                </div>
             </div>
         </header>
     );
